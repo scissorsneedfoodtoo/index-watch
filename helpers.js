1d9d915510fdc83f8843415879f7158211289842
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2018-12-24 00:09:15 +0900

    added closing date to check against in db later

--- helpers.js
+++ helpers.js
@@ -5,13 +5,14 @@ function scrapeIndex(html) {
   const name = $('#quote-header-info > div > div > div').text();
   const price = $('#quote-header-info > div > div > div > span').eq(1).text();
   const change = $('#quote-header-info > div > div > div > span').eq(2).text();
+  const closingDate = $('#Col1-1-HistoricalDataTable-Proxy > section > div > table > tbody > tr:nth-child(1) > td > span').eq(0).text();
 
   let ticker = name.match(/\(\w+\)/g);
   ticker = ticker[0].replace(/\(|\)/g, '');
 
   const [priceChange, percentChange] = change.split(' ');
 
-  return {ticker, price, priceChange, percentChange};
+  return {ticker, price, priceChange, percentChange, closingDate};
 }
 
 module.exports = {
